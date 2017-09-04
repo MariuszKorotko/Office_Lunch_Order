ec29cc436979c40cdf60a71bdf69e37c97fee645
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-09-04 20:09:04 +0200

    Test logout url

--- office_lunch_order/office_lunch_order_app/tests.py
+++ office_lunch_order/office_lunch_order_app/tests.py
@@ -3,7 +3,13 @@ from django.test import TestCase, Client
 c = Client()
 response = c.get('/officelunchorder/')
 response.status_code
+# 200
 response.content
 response = c.post('/officelunchorder/login/')
 response.status_code
+# 200
+response.content
+response = c.get('/officelunchorder/logout/')
+response.status_code
+# 200
 response.content
