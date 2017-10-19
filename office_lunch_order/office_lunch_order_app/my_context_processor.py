9b25a99a3bd4e820a35c75c6f78fa1b8b5019fb0
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-10-19 10:57:06 +0200

    PEP8

--- office_lunch_order/office_lunch_order_app/my_context_processor.py
+++ office_lunch_order/office_lunch_order_app/my_context_processor.py
@@ -1,9 +1,10 @@
 import datetime
 
+
 def footer_cp(request):
     """Display data time and first and last name of the owner."""
     context = {
                 "now": datetime.datetime.now(),
-                "owner" : "Mariusz Korotko"
+                "owner": "Mariusz Korotko",
               }
     return context
