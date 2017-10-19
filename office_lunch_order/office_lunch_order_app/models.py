82857474e8b1bd302c192b8bb3b59aeeeeb470c1
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-10-19 11:11:27 +0200

    PEP8

--- office_lunch_order/office_lunch_order_app/models.py
+++ office_lunch_order/office_lunch_order_app/models.py
@@ -6,7 +6,9 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=256)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
-                                 message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+                                 message="""Phone number must be entered in
+                                            the format: '+999999999'.
+                                            Up to 15 digits allowed.""")
     phone_number = models.CharField(max_length=64,
                                     validators=[phone_regex],
                                     blank=True)
